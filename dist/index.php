4981d6a92993538488f1395e957e42ae97460be6
Gem Barrett <gem@gembarrett.com>
2015-03-01 02:19:15 +0000

    Tidying up and adding comments

--- dist/index.php
+++ dist/index.php
@@ -1,17 +1,32 @@
 <?php
 // include './config.php';
+
+// get keys from files
 $privatekey = file_get_contents('./privkey');
+$apikey = file_get_contents('./apikey');
+
+// create the basic request
 $baseurl = 'http://gateway.marvel.com:80/v1/public/';
 $section = 'characters';
 $optional = '?name=hulk';
 $time = time();
-$apikey = file_get_contents('./apikey');
+
+// build hash of timestamp and keys
 $hash = '&hash='.hash('md5',$time.$privatekey.$apikey);
+
+// create request url
 $url = $baseurl.$section.$optional.'&ts='.$time.'&apikey='.$apikey.$hash;
+
+// get the json, chuck into a variable and decode
 $contents = file_get_contents($url);
 $results = json_decode($contents);
-$characterResults = $results->data->results;
+
+// chuck the json into a separate file - TODO: refactor so results get decoded and into file in fewer steps
 file_put_contents('results.json', $contents);
+
+// test out putting results on page
+$characterResults = $results->data->results;
+
 ?>
 
 
