32312d0b5098598ffffea526ffd9dab876d2028e
Gem Barrett <gem@gembarrett.com>
2015-03-01 01:25:57 +0000

    Read keys from separate files

--- dist/index.php
+++ dist/index.php
@@ -1,10 +1,11 @@
 <?php
-include './config.php';
+// include './config.php';
+$privatekey = file_get_contents('./privkey');
 $baseurl = 'http://gateway.marvel.com:80/v1/public/';
 $section = 'characters';
 $optional = '?name=hulk';
 $time = time();
-$apikey = 'e02430279a96009b4a899ad4e5e45b94';
+$apikey = file_get_contents('./apikey');
 $hash = '&hash='.hash('md5',$time.$privatekey.$apikey);
 $url = $baseurl.$section.$optional.'&ts='.$time.'&apikey='.$apikey.$hash;
 $contents = file_get_contents($url);
