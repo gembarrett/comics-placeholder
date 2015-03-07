8686155e4818002c59bb57dd96501fef0961feff
Gem Barrett <gem@gembarrett.com>
2015-03-07 11:27:13 +0100

    Test out getting all the image sizes available

--- dist/index.php
+++ dist/index.php
@@ -22,34 +22,36 @@ $contents = file_get_contents($url);
 $results = json_decode($contents);
 
 // chuck the json into a separate file - TODO: refactor so results get decoded and into file in fewer steps
-file_put_contents('results.json', $contents);
+// file_put_contents('results.json', $contents);
 
 // test out putting results on page
 $characterResults = $results->data->results;
 
-$imageSize = "detail";
+$imageSize = "portrait_small";
 
 $sizes = [
-  "portrait_small" => "50px x 75px",
-  "portrait_medium" => "100px x 150px",
-  "portrait_xlarge" => "150px x 225px",
-  "portrait_fantastic" => "168px x 252px",
-  "portrait_uncanny" => "300px x 450px",
-  "portrait_incredible" => "216px x 324px",
-  "standard_small" => "65px x 45px",
-  "standard_medium" => "100px x 100px",
-  "standard_large" => "140px x 140px",
-  "standard_xlarge" => "200px x 200px",
-  "standard_fantastic" => "250px x 250px",
-  "standard_amazing" => "180px x 180px",
-  "landscape_small" => "120px x 90px",
-  "landscape_medium" => "175px x 130px",
-  "landscape_large" => "190px x 140px",
-  "landscape_xlarge" => "270px x 200px",
-  "landscape_amazing" => "250px x 156px",
-  "landscape_incredible" => "464px x 261px",
-  "detail" => "full image constrained to 500px wide"
+  "portrait_small.jpg" => "50px x 75px",
+  "portrait_medium.jpg" => "100px x 150px",
+  "portrait_xlarge.jpg" => "150px x 225px",
+  "portrait_fantastic.jpg" => "168px x 252px",
+  "portrait_uncanny.jpg" => "300px x 450px",
+  "portrait_incredible.jpg" => "216px x 324px",
+  "standard_small.jpg" => "65px x 45px",
+  "standard_medium.jpg" => "100px x 100px",
+  "standard_large.jpg" => "140px x 140px",
+  "standard_xlarge.jpg" => "200px x 200px",
+  "standard_fantastic.jpg" => "250px x 250px",
+  "standard_amazing.jpg" => "180px x 180px",
+  "landscape_small.jpg" => "120px x 90px",
+  "landscape_medium.jpg" => "175px x 130px",
+  "landscape_large.jpg" => "190px x 140px",
+  "landscape_xlarge.jpg" => "270px x 200px",
+  "landscape_amazing.jpg" => "250px x 156px",
+  "landscape_incredible.jpg" => "464px x 261px",
+  "detail.jpg" => "full image constrained to 500px wide"
 ];
+
+$imageURL = $characterResults[0]->thumbnail->path."/";
 
 ?>
 
@@ -67,11 +69,10 @@ $sizes = [
   <?php print_r($results->attributionHTML);?>
   <p><?php print_r($characterResults[0]->name);?></p>
   <p><?php print_r($characterResults[0]->description);?></p>
-  <img src="<?php echo $characterResults[0]->thumbnail->path."/".$imageSize.".jpg";?>" />
   <ul>
   <?php
-  foreach ($sizes as $value) {
-      echo "<li>$value</li>";
+  foreach ($sizes as $key => $value) {
+      echo "<img src='$imageURL$key' />";
   }
   ?>
   </ul>
